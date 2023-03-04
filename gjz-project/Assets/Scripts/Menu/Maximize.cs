a0ebc1bff3b067b1f6a600ec0434bae7d4858b98
Lukáš Urban <lukas.urbann@seznam.cz>
2023-03-04 19:36:04 +0100

    sounds, maximize button

---
+++ gjz-project/Assets/Scripts/Menu/Maximize.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using UI;
+
+namespace Menu
+{
+    public class Maximize : Button
+    {
+        public override void ButtonAction()
+        {
+            Screen.fullScreen = !Screen.fullScreen;
+            
+            if(!Screen.fullScreen)
+                Screen.SetResolution(Screen.currentResolution.width, Screen.currentResolution.height, FullScreenMode.FullScreenWindow);
+            else
+                Screen.SetResolution(800, 600, FullScreenMode.Windowed);
+        }
+    }
+}
