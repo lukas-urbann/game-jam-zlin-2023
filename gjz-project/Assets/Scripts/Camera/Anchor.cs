880ae560c2e2cc265559beb1e63f5cdd8f052566
Lukáš Urban <lukas.urbann@seznam.cz>
2023-03-04 14:40:12 +0100

    stable

--- gjz-project/Assets/Scripts/Camera/Anchor.cs
+++ gjz-project/Assets/Scripts/Camera/Anchor.cs
@@ -9,13 +9,14 @@ namespace Camera
     {
         //Target je objekt či transform, jehož pozici kamera následuje.
         [SerializeField] private Transform target;
+        [SerializeField] private float speed;
         
         private void Update()
         {
             transform.position = new Vector3(
-                Mathf.Lerp(transform.position.x, target.transform.position.x, 2 * Time.deltaTime),
-                Mathf.Lerp(transform.position.y, target.transform.position.y, 2 * Time.deltaTime),
-                Mathf.Lerp(transform.position.z, target.transform.position.z, 2 * Time.deltaTime)
+                Mathf.Lerp(transform.position.x, target.transform.position.x, speed * Time.deltaTime),
+                Mathf.Lerp(transform.position.y, target.transform.position.y, speed * Time.deltaTime),
+                Mathf.Lerp(transform.position.z, target.transform.position.z, speed * Time.deltaTime)
                 );
         }
     }
